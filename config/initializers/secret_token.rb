313e3928397d7cb3f5e45b22c148e8ecbfea65b7
Shaun Johnson <shaunejohnson@Shaun-E-Johnsons-MacBook-Pro.local>
2012-06-09 22:17:25 -0400

    Generated Rails App
    Installed bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shaundotme::Application.config.secret_token = 'd7ba9642a051138d6eaf16a22c236ecbc6a6bb5bfb49cbcb2b8623a190f02a247253ea9caca634dd9e903bb82ae85b1902257bdc06b1df09e4402f527c73e657'
